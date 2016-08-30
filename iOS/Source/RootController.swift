cdf67b8409be9362d7b176d53ec2e93e808e8d4f
Elvis Nuñez <elvisnunez@me.com>
2016-08-30 23:11:12 +0200

    Remove boilerplate

--- iOS/Source/RootController.swift
+++ iOS/Source/RootController.swift
@@ -1,11 +1,6 @@
 import UIKit
 
-class RootController: BaseViewController {
-    override func loadView() {
-        let view = RootView(frame: UIScreen.mainScreen().bounds)
-        self.view = view
-    }
-
+class RootController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
